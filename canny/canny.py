6b2b1f7e88b206a49535e22c8b04d84d56bfb0ef
Stefan Berger <s.berger.bmio@gmail.com>
2021-06-01 15:10:14 +0200

    result images

--- canny/canny.py
+++ canny/canny.py
@@ -7,7 +7,7 @@ parser.add_argument("--input", required=True, help="path to image")
 parser.add_argument("--output", required=True, help="path to output image")
 args = parser.parse_args()
 
-img = cv.imread(args.input, 0)
+img = cv.imread(args.input, cv.IMREAD_GRAYSCALE)
 edges = cv.Canny(img, 100, 200)
 edges = (255 - edges)
 # plt.subplot(121), plt.imshow(img, cmap='gray')
@@ -16,3 +16,8 @@ edges = (255 - edges)
 # plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 # plt.show()
 cv.imwrite(args.output, edges)
+
+# import os
+# import cv2 as cv
+# for filename in os.listdir('tables'):
+#     cv.imwrite(os.path.join('edges', filename), 255 - cv.Canny(cv.imread(os.path.join('tables', filename)), 100, 200))
