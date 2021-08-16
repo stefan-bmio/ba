f1653a931f1766180706bcb409f9a5bd4e4a36f3
Stefan Berger <s.berger.bmio@gmail.com>
2021-08-16 23:31:28 +0200

    candles

--- QuickDraw2Obj/ndjson2jpg.py
+++ QuickDraw2Obj/ndjson2jpg.py
@@ -4,17 +4,17 @@ import tensorflow as tf
 import json
 import matplotlib.pyplot as plt
 
-dataset = tf.data.TextLineDataset(['table.ndjson'])
+dataset = tf.data.TextLineDataset(['../candle.ndjson'])
 
 for index, line in enumerate(dataset):
     if index % 1000 == 0:
-        print(str(index) + ' of ~128000')
+        print(str(index) + ' of ~140000')
     jsonLine = json.loads(line.numpy())
     drawing = jsonLine['drawing']
     for stroke in drawing:
         plt.plot(stroke[0], [255 - y for y in stroke[1]], 'k')
         plt.axis('off')
-    plt.savefig(os.path.join('images', str(index) + '.jpg'))
+    plt.savefig(os.path.join('candles', str(index) + '.jpg'))
 
     with open(os.path.join('json', str(index) + '.json'), 'w') as file:
         file.write(json.dumps(drawing))
---
+++ QuickDraw2Obj/ndjson2jpg_PIL.py
@@ -0,0 +1,33 @@
+import os
+from functools import reduce
+import tensorflow as tf
+import json
+from PIL import Image, ImageDraw
+
+dataset = tf.data.TextLineDataset(['../candle.ndjson'])
+
+for index, line in enumerate(dataset):
+    if index % 1000 == 0:
+        print(str(index) + ' of ~140000')
+    jsonLine = json.loads(line.numpy())
+    drawing = jsonLine['drawing']
+
+    im = Image.new('RGB', (256, 256), (255, 255, 255))
+    draw = ImageDraw.Draw(im)
+
+    # normalise coords to center the drawing
+    x_coords = reduce(lambda a, b: a + b, [stroke[0] for stroke in drawing])
+    displacement_x = int((256 - min(x_coords) - max(x_coords)) / 2)
+    y_coords = reduce(lambda a, b: a + b, [stroke[1] for stroke in drawing])
+    displacement_y = int((256 - min(y_coords) - max(y_coords)) / 2)
+
+    for stroke in drawing:
+        draw.line(list(zip([x + displacement_x for x in stroke[0]], [y + displacement_y for y in stroke[1]])), fill=0)
+
+    im.save(os.path.join('candles', str(index) + '.jpg'), 'JPEG')
+
+    with open(os.path.join('json', str(index) + '.json'), 'w') as file:
+        file.write(json.dumps(drawing))
+
+    if index == 5000:
+        break
